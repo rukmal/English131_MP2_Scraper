a47581f12ff755dfadd211e67d0c6745bbb8b050
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-28 14:04:45 -0700

    Script working correctly now. Grabbing reddit articles correctly. Now all that is left is to set up redis

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 from flask import Flask
 from bs4 import BeautifulSoup
 import urllib
+import redis
 
 app = Flask(__name__)
 
 REDDIT_BASE_URL = 'http://reddit.com/r/ads'
 
-# def getVoteDelta()
+def getVoteDelta(divAttrsDict):
+	upVotes = int(divAttrsDict['data-ups'])
+	downVotes = int(divAttrsDict['data-downs'])
+	return upVotes - downVotes
+
+def getSourceLink(divAttrsDict):
+	for link in divAttrsDict.find_all('a'):
+		try: # this is because some of the links are blank
+			if 'may-blank' in link.get('class'):
+				return link.get('href')
+		except:
+			pass
 
 def getImages():
 	adPage = BeautifulSoup(urllib.urlopen(REDDIT_BASE_URL).read())
 	for div in adPage.find_all('div'):
 		if div.get('id') == 'siteTable':
 			counter = 0
-			maxCount = 10
+			maxCount = 20
 			for childDiv in div:
 				if counter == maxCount:
 					break
 				if 'thing' in childDiv.get('class'):
-					# voteDelta = # function taking params
-					# sourceLink = # function taking params
-					# redditLink = # function taking params
-					print childDiv.attrs
+					voteDelta = getVoteDelta(childDiv.attrs)
+					sourceLink = getSourceLink(childDiv)
+					print 'Source link: ' + sourceLink + ' with a vote delta of ' + str(voteDelta)
 				counter += 1
-			print counter
 
 getImages()
 
